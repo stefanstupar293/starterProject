d6b19360c7327fa76430921c1c08a4e18b4b0203
stefanstupar293 <stefanstupar@gmail.com>
2019-09-20 16:55:38 +0200

    Vaadin designer finished
    Started to implement login backend, created mock email/pass data.

--- my-starter-project/frontend/src/views/login/login-test.js
+++ my-starter-project/frontend/src/views/login/login-test.js
@@ -2,10 +2,10 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-text-field/src/vaadin-email-field.js';
 import '@vaadin/vaadin-text-field/src/vaadin-password-field.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-login/src/vaadin-login-form.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
 class LoginTest extends PolymerElement {
 
@@ -18,13 +18,15 @@ class LoginTest extends PolymerElement {
                 }
             </style>
 <vaadin-vertical-layout style="width: 100%; height: 100%;">
- <vaadin-text-field label="Email:" placeholder="User Email" id="emailInput" required="true"></vaadin-text-field>
- <vaadin-password-field label="Password:" placeholder="Password:" id="passInput" required="true"></vaadin-password-field>
- <vaadin-horizontal-layout>
-  <vaadin-button id="btnLogin" theme="raised primary">
+ <h2 style="align-self: center" id="h2">Beverage Buddy Login</h2>
+ <vaadin-text-field label="Email:" placeholder="User Email" id="emailInput" required="true" autofocus="true" style="align-self: center"></vaadin-text-field>
+ <vaadin-password-field label="Password:" placeholder="Password" id="passInput" required="true" style="align-self: center"></vaadin-password-field>
+ <vaadin-horizontal-layout style="align-self: center">
+  <vaadin-button id="btnLogin" theme="raised primary" style="justify-content: start">
     Login 
   </vaadin-button>
-  <vaadin-button id="btnCancel" theme="raised secondary">
+  <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout" style="width: 25px"></vaadin-vertical-layout>
+  <vaadin-button id="btnCancel" theme="raised secondary" style="justify-content: end">
     Cancel 
   </vaadin-button>
  </vaadin-horizontal-layout>
---
+++ my-starter-project/src/main/java/com/gmail/stefan/ui/views/login/AppUserServices.java
@@ -0,0 +1,10 @@
+package com.gmail.stefan.ui.views.login;
+
+import java.io.Serializable;
+
+public class AppUserServices implements Serializable {
+	
+	private static final String MOCK_USERNAME = "admin";
+	private static final String MOCK_PASSWORD = "admin";
+	
+}
