a0dc6deb7d022459db5410a785dca616bbc2ff0b
stefanstupar293 <stefanstupar@gmail.com>
2019-10-11 12:49:01 +0200

    connected to DB
    The "todo" db data is accessible.

--- my-starter-project/src/main/java/com/gmail/stefan/backend/dbservices/TestDBConnection.java
+++ my-starter-project/src/main/java/com/gmail/stefan/backend/dbservices/TestDBConnection.java
@@ -3,32 +3,38 @@ package com.gmail.stefan.backend.dbservices;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 
 public class TestDBConnection {
 	 
-	public static void main (String[] args) throws Exception {
-		System.out.println("Connected");
-		
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/todo", "root", "komkom");
-//			Connection com = DriverManager.getConnection("java:/comp/env/jdbc/todo", "root", "komkom");
-			System.out.println("Connected");
-			Statement st = con.createStatement();
+	public static void main (String[] args) throws SQLException {
+			System.out.println("Connecting...");
+			
+		 	String query = "select firstname from author where id=28";
+//		 	String query = "insert into author values (32, 'Abraham', '2019-01-14 10:45:51')";
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://46.4.55.195:3306/todo?serverTimezone=UTC#", "root", "komkom");
 			System.out.println("Connection succsessful!");
-			ResultSet rs = st.executeQuery("select firstname from author where id=28 ");
+			Statement st = con.createStatement();
+			
+			ResultSet rs = st.executeQuery(query);
+//			int count = st.executeUpdate(query);
 			
 			String name = "";
 			
 			while(rs.next()) {
 				
-			int roll = rs.getInt("id");
+			
 			name = rs.getString("firstname");
-			System.out.println(roll+ " "+name);
+			System.out.println("Retrieved user name is: " + name);
 			}
+//			System.out.println(count + " row/s affected");
 			
 			st.close();
 			con.close();
+			System.out.println("Connection closed. ");
 	}
 	
 	
